9463a63a9cd94f886d5b1a3ce1f9baf244bb596c
Artur Lopatniuk <93662061+ArturAndreevich@users.noreply.github.com>
2023-05-09 11:51:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, DevOps-167!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Artur Lopatniuk!
         </a>
       </header>
     </div>
